b16d9db49c5467f4030509d51d834cc3477148b5
Jack Brewster <jb803@cam.ac.uk>
2018-10-03 21:09:51 +0100

    Initial pass at setting up Travis CI and coveralls
    Tox is known to behae badly with anaconda. Where tox would normally be used
    I've substituted a bash script. To simulate what the TavisCI agent will run use
    ./provision/travis/run-tests.sh
    
    It would be good to find a tox alternative. (Or failing that we could use a different method
    of installing dependencies e.g. docker or using the apt-get version of FENICS...)

---
+++ provision/travis/run-tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Run tests on Travis CI worker.
+
+#Exit on error
+set -e
+#enable logging
+set -x
+
+if [ -z "$TEST_ENV" ]; then
+#No test environment is set, we run all the tests, build the docs and do black
+    pytest --cov=tonic/ tonic test
+    sphinx-build -b html doc/source/ doc/build/
+    black --check tonic/
+elif [ "$TEST_ENV" = "py36-test" ]; then
+    pytest --cov=tonic/ tonic test
+elif [ "$TEST_ENV" = "doc" ]; then
+    sphinx-build -b html doc/source/ doc/build/
+elif [ "$TEST_ENV" = "black" ]; then
+     black --check tonic/
+else
+    echo Uknown test environment
+fi
---
+++ test/test_deps.py
@@ -0,0 +1,4 @@
+"""Tests  various dependencies. This is an initial example test required to allow pytest to be happy when there are no actual tests written """
+
+def test_importDolfin():
+    import dolfin as dolf
