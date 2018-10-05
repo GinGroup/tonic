6683381c2c1c4047dfa589a1d2370fabbf363136
corwinpro <corwinat@gmail.com>
2018-10-05 22:13:29 +0100

    black corrections

--- test/test_parameters.py
+++ test/test_parameters.py
@@ -1,26 +1,29 @@
 from tonic.parameters import Parameter, ChildParameter
 
+
 def test_addition_1():
-    p1 = Parameter(0., is_variable=False)
-    p2 = Parameter(1.)
-    assert (p1 + p2 == 1.)
-    assert (p1 - p2 == -1.)
+    p1 = Parameter(0.0, is_variable=False)
+    p2 = Parameter(1.0)
+    assert p1 + p2 == 1.0
+    assert p1 - p2 == -1.0
+
 
 def test_child():
-    p1 = Parameter(0., is_variable=False)
-    p2 = Parameter(1.)
+    p1 = Parameter(0.0, is_variable=False)
+    p2 = Parameter(1.0)
 
-    child = p1 + 3.
-    assert(child.value == 3.)
-    assert(child.parent == p1)
-    assert(child.parent != p2)
+    child = p1 + 3.0
+    assert child.value == 3.0
+    assert child.parent == p1
+    assert child.parent != p2
+
 
 def test_child_of_child():
-    p1 = Parameter(0., is_variable=False)
-    p2 = Parameter(1.)
+    p1 = Parameter(0.0, is_variable=False)
+    p2 = Parameter(1.0)
 
-    child = p1 + 3.
+    child = p1 + 3.0
 
-    child2 = child * 2.
-    assert(child2.value == 6.)
-    assert(child2.parent == p1)
+    child2 = child * 2.0
+    assert child2.value == 6.0
+    assert child2.parent == p1
